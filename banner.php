c1b6677ac184407ba2e1151119f11646124ba469
Geovani Figueiredo <geovani537@gmail.com>
2025-11-17 18:59:56 -0300

    ajust: cleaning spaces and identations

--- banner.php
+++ banner.php
@@ -1,94 +1,83 @@
 <?php
 
-
 function get_custom_banner() {
-	
-	global $wpdb;
+    global $wpdb;
     
 	if( is_search() || is_404() ) {
         echo '';
         return;
     }
 
-        // Set a default image URL
-        $banner_image_url = 'https://www.farmacia.ufmg.br/wp-content/uploads/2015/06/fafar1.jpg';
-        $title = '';
-        $subtitle = '';
-
-        $slider = '';
+    // Set a default image URL
+    $banner_image_url = 'https://www.farmacia.ufmg.br/wp-content/uploads/2015/06/fafar1.jpg';
+    $title            = '';
+    $subtitle         = '';
+    $slider           = '';
 
     $category = institutional_fafar_get_category();
 
     if( $category ) {
         $slider = $wpdb->get_row( "SELECT * FROM wp_revslider_sliders WHERE alias = '$category->slug'" );
 
-            // Banner Content
-            $title  = $category->description;
+        // Banner Content
+        $title  = $category->description;
+    } else {
+        $post_data = get_queried_object();
 
-        } else {
-
-            $post_data = get_queried_object();
-
-            if ( $post_data !== null ) {
-                
-                $author_id = $post_data->post_author;
-        
-                $author_nicename = get_the_author_meta('user_nicename', $author_id);
-
-                $slider = $wpdb->get_row( "SELECT * FROM wp_revslider_sliders WHERE alias = '$author_nicename'" );
-
-                // Banner Content
-                $author_category = get_category_by_slug($author_nicename);
-
-                if ( ! empty( $author_category ) ) {
-                    $title = $author_category->description;
-                }
-            }
-
-        }
-
-        if( isset( $slider->id ) )
-            $slides = $wpdb->get_results( "SELECT * FROM wp_revslider_slides WHERE slider_id = $slider->id" );
-
-        if( isset( $slides ) && count( $slides ) > 0 && !is_home() ) {
-
-            $newest_slide =  end( $slides );
-
-            $image_slide_url = json_decode( $newest_slide->params )->image;
-
-            $relative_image_slide_url = explode( 'wp-content', $image_slide_url )[1];
-
-            $banner_image_url = site_url( '/wp-content' . $relative_image_slide_url, 'https' );
-
-        }
-   
-        if ( $banner_image_url == 'https://www.farmacia.ufmg.br/wp-content/uploads/2015/06/fafar1.jpg' ) {
-            
-            $title = '';
-            $subtitle = '';
-            $banner_image_url = get_stylesheet_directory_uri() . '/assets/img/fafar-vista-aerea.jpeg';
-
-        }
-
-        $autoplay = null;
-        if ( is_home() ) {
-
-            $autoplay = 'true';
-
-        }
-
-        $html = '<div id="banner-container" class="ast-container" data-autoplay="' . $autoplay . '">' .
-                    '<div class="page-banner-container banner-image-custom-transition" style="background-image: url(' . $banner_image_url . ');">' .
-                            '<div class="page-banner-body">' .
-                                '<h2 class="page-banner-title">' . $title  . '</h2>' .
-                                '<h3 class="page-banner-subtitle">' . $subtitle  . '</h3>' .
-                            '</div>' .
-                    '</div>' .
-                '</div>';
-
+        if ( $post_data !== null ) {
+            $author_id = $post_data->post_author;
     
-        echo $html;
+            $author_nicename = get_the_author_meta('user_nicename', $author_id);
+
+            $slider = $wpdb->get_row( "SELECT * FROM wp_revslider_sliders WHERE alias = '$author_nicename'" );
+
+            // Banner Content
+            $author_category = get_category_by_slug($author_nicename);
+
+            if ( ! empty( $author_category ) ) {
+                $title = $author_category->description;
+            }
+        }
     }
+
+    if( isset( $slider->id ) ) {
+        $slides = $wpdb->get_results( "SELECT * FROM wp_revslider_slides WHERE slider_id = $slider->id" );
+    }
+
+    if( isset( $slides ) && count( $slides ) > 0 && ! is_home() ) {
+        $newest_slide = end( $slides );
+
+        $image_slide_url = json_decode( $newest_slide->params )->image;
+
+        $relative_image_slide_url = explode( 'wp-content', $image_slide_url )[1];
+
+        $banner_image_url = site_url( '/wp-content' . $relative_image_slide_url, 'https' );
+    }
+
+    if ( $banner_image_url == 'https://www.farmacia.ufmg.br/wp-content/uploads/2015/06/fafar1.jpg' ) {
+        $title = '';
+
+        $subtitle = '';
+
+        $banner_image_url = get_stylesheet_directory_uri() . '/assets/img/fafar-vista-aerea.jpeg';
+    }
+
+    $autoplay = null;
+    if ( is_home() ) {
+        $autoplay = 'true';
+    }
+
+    $html = '<div id="banner-container" class="ast-container" data-autoplay="' . $autoplay . '">' .
+                '<div class="page-banner-container banner-image-custom-transition" style="background-image: url(' . $banner_image_url . ');">' .
+                        '<div class="page-banner-body">' .
+                            '<h2 class="page-banner-title">' . $title  . '</h2>' .
+                            '<h3 class="page-banner-subtitle">' . $subtitle  . '</h3>' .
+                        '</div>' .
+                '</div>' .
+            '</div>';
+
+
+    echo $html;
 }
 
 add_action( 'astra_content_before', 'get_custom_banner' );
